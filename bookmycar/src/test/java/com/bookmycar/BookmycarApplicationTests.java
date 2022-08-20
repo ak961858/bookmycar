cfc2c3f70e4ba16e820c7c4bbddee3bac7e333e4
UVKDY6I <KumarAditya@JohnDeere.com>
2022-08-20 10:56:03 +0530

    first commit

---
+++ bookmycar/src/test/java/com/bookmycar/BookmycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
