b9a5f0068832553c2ac49355442b58e314e72424
UVKDY6I <KumarAditya@JohnDeere.com>
2022-08-20 22:01:58 +0530

    remove merge conflicts

--- bookmycar/src/main/java/com/bookmycar/service/CarService.java
+++ bookmycar/src/main/java/com/bookmycar/service/CarService.java
@@ -3,16 +3,12 @@ package com.bookmycar.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.smartcardio.CardNotPresentException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookmycar.dao.CarRepository;
 import com.bookmycar.exceptions.CarNotFoundException;
-import com.bookmycar.exceptions.UserNotFoundException;
 import com.bookmycar.model.Car;
-import com.bookmycar.model.User;
 
 @Service
 public class CarService {
